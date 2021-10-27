0af0e2e8a3447356b9d3669c548bc4791471d0ec
JoshuaT <jttsat170@gmail.com>
2021-10-27 14:41:38 -0400

    new

--- models/post.js
+++ models/post.js
@@ -1,5 +1,4 @@
-
-const { Sequelize, Model, DataTypes } = require('sequelize');
+const {Sequelize, Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 class Post extends Model {}
--- utils/auth.js
+++ utils/auth.js
@@ -1,22 +1,10 @@
-<<<<<<< HEAD
-//copied from 14 min-project
-const withAuth = (req, res, next) => {
-    // If the user is not logged in, redirect the request to the login route
-    if (!req.session.logged_in) {
-      res.redirect('/login');
-=======
 const withAuth = (req, res, next) => {
     if (!req.session.userId) {
       res.redirect("/login");
->>>>>>> 28330db49ff13e65b58b7b7d81c4bd8b7cf106c4
     } else {
       next();
     }
   };
   
-<<<<<<< HEAD
-  module.exports = withAuth;  
-=======
   module.exports = withAuth;
   
->>>>>>> 28330db49ff13e65b58b7b7d81c4bd8b7cf106c4
