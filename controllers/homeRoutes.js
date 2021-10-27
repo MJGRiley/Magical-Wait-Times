f6753d70a846849e6faa406cc3d84d7d9af93f16
JoshuaT <jttsat170@gmail.com>
2021-10-27 11:02:14 -0400

    userRoutes

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,23 @@
+const router = require('express').Router();
+
+
+
+router.get('/login', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+    if(req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('signup');
+});
+
+module.exports = router;
