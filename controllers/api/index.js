ea094ed4beb5a10234437867f0d2dea511252087
JoshuaT <jttsat170@gmail.com>
2021-10-29 17:36:01 -0400

    newroutes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const userR = require('./userRoutes');
 const postR = require('./post');
 const rideR = require('./ride')
+const landR = require('/land');
 
 router.use('/users', userR);
 router.use('/post', postR);
 router.use('/ride', rideR);
+router.use('/land', landR);
 
 module.exports = router
