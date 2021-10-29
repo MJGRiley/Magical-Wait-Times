c1112c851c3a713bc15e0200b5498afd7c8c909d
Morgan Riley <89412679+MJGRiley@users.noreply.github.com>
2021-10-29 11:16:26 -0400

    Update userRoutes.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,14 +1,7 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
-
-
-router.post('/', withAuth, async (req, res) => {
-    try{
-        const newUser = await User.create({
-            username: req.body.username,
-            password: req.body.password,
-            
+        
 router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
